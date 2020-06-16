8c04866ddc5dacc2d7e4588b2eb32334da628109
claudiosfcarvalho <claudiosfcarvalho@outlook.com>
2020-06-16 17:34:09 -0300

    implementando edição e exclusao

--- src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -24,9 +25,11 @@ public class TituloController {
 	@Autowired
 	private Titulos titulos;
 
+	private static final String  CADASTRO_VIEW = "CadastroTitulo";
+	
 	@RequestMapping("/novo")
 	public ModelAndView novo() {
-		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
 		mv.addObject(new Titulo());
 		return mv;
 	}
@@ -48,6 +51,20 @@ public class TituloController {
 		ModelAndView mv = new ModelAndView("PesquisaTitulos");
 		mv.addObject("titulos", todosTitulos);
 		return mv;
+	}
+	
+	@RequestMapping("/{id}")
+	public ModelAndView edicao(@PathVariable("id") Titulo titulo) {
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
+		mv.addObject(titulo);
+		return mv;
+	}
+	
+	@RequestMapping(value="/{id}",method=RequestMethod.DELETE)
+	public String excluir(@PathVariable Long id, RedirectAttributes attibutes) {
+		titulos.delete(id);
+		attibutes.addFlashAttribute("mensagem", "Título excluído com sucesso!");
+		return "redirect:/titulos";
 	}
 	
 	@ModelAttribute("todosStatusTitulo")
